724a5520dd4a5111178f2150aba25448655abf80
Igor Ostapenko <gary.nika@gmail.com>
2022-02-02 20:48:39 +0200

    l3

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@mui/material/CssBaseline'
+
+export const Menu = () => {
+    return (
+        <>
+<Button color="inherit">Home</Button>
+<Button color="inherit">Product</Button>
+<Button color="inherit">Home</Button>
+<Button color="inherit">Home</Button>
+    )
+}
