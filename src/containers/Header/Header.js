c40315f1e12775d9df239837c8e3e1f3e015ad92
Igor Ostapenko <gary.nika@gmail.com>
2022-02-02 22:59:56 +0200

    l3

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -2,13 +2,11 @@ import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
+
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
 import { Menu } from 'components/Menu/Menu'
-
-
 
 export const Header = () => {
     return (
