672e7d3f0320138e349607164f4a34a19306a310
Igor Ostapenko <gary.nika@gmail.com>
2022-02-02 21:19:26 +0200

    l3

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,7 +6,7 @@ export const Menu = () => {
         <>
 <Button color="inherit">Home</Button>
 <Button color="inherit">Product</Button>
-<Button color="inherit">Home</Button>
-<Button color="inherit">Home</Button>
+<Button color="inherit">Payment</Button>
+<Button color="inherit">Shipping</Button>
     )
 }
