a26e83445fca3f66b718da7d1f3a68e25ec1339f
Igor Ostapenko <gary.nika@gmail.com>
2022-02-04 17:43:11 +0200

    save styled-components

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import 'components/Cart/CartHeader.scss'
 
 export const CartHeader = () => {
     return (
         <div>
-            <div>0</div>
+            <div className='red'>0</div>
             <div>$0</div>
         </div>
     )
