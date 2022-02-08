729d393972e7106558533fd95b1b1552a84fce11
Igor Ostapenko <gary.nika@gmail.com>
2022-02-08 17:39:13 +0200

    l4

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles({
+    title: {
+        margin: '30px 0',
+        textTransform: 'uppercase',
+    },
+})
 
 export const ProductsList = () => {
+    const classes = useStyles()
     return (
         <>
             <Typography
                 align="center"
                 component="h1"
                 variant="h4"
-                style={{
-                    textTransform: 'uppercase',
-                    margin: '30px 0',
-                }}
+                className={classes.title}
             >
                 Products List
             </Typography>
