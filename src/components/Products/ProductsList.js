1f48b1518546702d0562b6fa1eb4021896d24279
Igor Ostapenko <gary.nika@gmail.com>
2022-02-09 13:31:35 +0200

    l4

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -3,6 +3,7 @@ import Typography from '@mui/material/Typography'
 import { makeStyles } from '@mui/styles'
 import { Grid } from '@mui/material'
 import { ProductsListItem } from './ProductsListItem'
+import { ProductsArray } from './productsArray'
 
 const useStyles = makeStyles({
     title: {
@@ -24,23 +25,24 @@ export const ProductsList = () => {
                 Products List
             </Typography>
 
-            <Grid 
-            container
-            direction="row"
-            justifyContent="space-evenly"
-            alignItems="center"
-            spacing={3}
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-evenly"
+                alignItems="center"
+                spacing={3}
             >
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem/>
-                </Grid>
-
+                {ProductsArray.map((product) => (
+                    <Grid item xs={12} sm={6} md={4}>
+                        <ProductsListItem
+                            name={product.name}
+                            description={product.description}
+                            capacity={product.capacity}
+                            type={product.type}
+                            price={product.price}
+                        />
+                    </Grid>
+                ))}
             </Grid>
         </>
     )
--- src/components/Products/ProductsListItem.js
+++ src/components/Products/ProductsListItem.js
@@ -1,16 +1,16 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
 import React from 'react'
 
-export const ProductsListItem = () => {
+export const ProductsListItem = (props) => {
     return (
         <>
             <Card>
                 <CardContent>
-                    <h4>iPhone X</h4>
-                    <p>This is iPhone X</p>
-                    <div>Type: phone</div>
-                    <div>Capacity: 64Gb</div>
-                    <div>500 $</div>
+                    <h4>{props.name}</h4>
+                    <p>{props.description}</p>
+                    <div>Type: {props.type}</div>
+                    <div>Capacity: {props.capacity}</div>
+                    <div>{props.price} $</div>
                 </CardContent>
                 <CardActions>
                     <Button variant="outlined">Add to cart</Button>
