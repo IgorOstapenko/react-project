28ce08fa514f1a940262e0e8ec010e61d6809c19
Igor Ostapenko <gary.nika@gmail.com>
2022-02-09 14:05:51 +0200

    l3

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -4,7 +4,7 @@ import 'components/Cart/CartHeader.scss'
 export const CartHeader = () => {
     return (
         <div>
-            <div className='red'>0</div>
+            <div>0</div>
             <div>$0</div>
         </div>
     )
