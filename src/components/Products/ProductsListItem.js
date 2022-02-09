8d152eb4607fba31451baa7c44e0763389da40ba
Igor Ostapenko <gary.nika@gmail.com>
2022-02-09 11:29:23 +0200

    l4

--- src/components/Products/ProductsListItem.js
+++ src/components/Products/ProductsListItem.js
@@ -1,2 +1,21 @@
-
 import { Button, Card, CardActions, CardContent } from '@mui/material'
+import React from 'react'
+
+export const ProductsListItem = () => {
+    return (
+        <>
+            <Card>
+                <CardContent>
+                    <h4>iPhone X</h4>
+                    <p>This is iPhone X</p>
+                    <div>Type: phone</div>
+                    <div>Capacity: 64Gb</div>
+                    <div>500 $</div>
+                </CardContent>
+                <CardActions>
+                    <Button>Add to cart</Button>
+                </CardActions>
+            </Card>
+        </>
+    )
+}
